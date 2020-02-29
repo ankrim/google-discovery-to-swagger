4d17067d0059110a38f83fdc458c5a66273e04c8
Morad <ankrim@transposit.com>
2020-02-28 18:30:53 -0800

    try different discovary urls

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 const Google2Swagger = require('./index');
+const _ = require('lodash');
 const axios = require('axios');
 const yaml = require('js-yaml');
+
+const MAX_API_VERSION = 5;
+const service = process.argv[2];
+const maybeSpecificVersion = process.argv[3];
+
+const possibleDiscoveryUrls = buildPossibleDiscoveryUrls();
+
+function buildPossibleDiscoveryUrls() {
+  if (maybeSpecificVersion) {
+    return getDiscoveryUrls(maybeSpecificVersion);
+  }
+
+  return _.flatten(_.range(1, MAX_API_VERSION + 1).map(version => getDiscoveryUrls(`v${version}`)));
+}
+
+function getDiscoveryUrls(version) {
+  return [
+    `https://${service}.googleapis.com/$discovery/rest?version=${version}`,
+    `https://www.googleapis.com/discovery/v1/apis/${service}/${version}/rest`
+  ]
+}
 
 function convertGoogleDiscoveryToSwagger(response) {
   const swagger2Json = Google2Swagger.convert(response.data);
@@ -12,20 +34,19 @@ function convertGoogleDiscoveryToSwagger(response) {
   console.log(swagger2Yaml);
 };
 
-function getApiDiscovery(serviceUrl, version) {
-  if (version > 5){
-    throw "can't find api discovery for version 1-5.";
+function getApiDiscovery(index) {
+  if (index > possibleDiscoveryUrls.length){
+    console.error("can't find api discovery.");
+    return;
   }
 
-  const discoveryUrl = `https://${serviceUrl}/$discovery/rest?version=v${version}`;
+  const discoveryUrl = possibleDiscoveryUrls[index];
 
   axios.get(discoveryUrl)
-  .then(convertGoogleDiscoveryToSwagger)
-  .catch(function (error) {
-    getApiDiscovery(serviceUrl, version + 1);
-  });
+    .then(convertGoogleDiscoveryToSwagger)
+    .catch(function (error) {
+      getApiDiscovery(index + 1);
+    });
 }
 
-const apiUrl = process.argv[2];
-
-getApiDiscovery(apiUrl, 1);
+getApiDiscovery(0);
